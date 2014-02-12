9405aa97341811000a36a7c470169ee9c08c72fd
Dominik Schadow <dominikschadow@googlemail.com>
2014-02-12 17:38:20 +0100

    Create user (incomplete)

--- FerrisWheelManager/src/main/java/de/dominikschadow/fwm/session/UserBean.java
+++ FerrisWheelManager/src/main/java/de/dominikschadow/fwm/session/UserBean.java
@@ -2,11 +2,30 @@ package de.dominikschadow.fwm.session;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 @ManagedBean
 @RequestScoped
 public class UserBean {
+    private User user = new User();
+
+    @PersistenceContext(unitName = "fwm")
+    private EntityManager em;
+
+    @Transactional(Transactional.TxType.REQUIRED)
     public String createUser() {
+        em.persist(user);
+
         return "/users/index";
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
