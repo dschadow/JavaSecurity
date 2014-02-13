4477a8d339993828486a824d6a7aeeb6fe849f69
Dominik Schadow <dominikschadow@googlemail.com>
2014-02-13 17:51:12 +0100

    SQL scripts for database setup

---
+++ Resources/FerrisWheelManager/Database/FERRIS_WHEELS.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `FERRIS_WHEELS` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `location` varchar(50) NOT NULL,
+  `speed` int(11) DEFAULT NULL,
+  `installation_date` date DEFAULT NULL,
+  `maintenance_date` date DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  CONSTRAINT `ferris_wheel_owner` FOREIGN KEY (`id`) REFERENCES `USERS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
+insert into ferris_wheels (name, description, location, speed,
+installation_date, maintenance_date, user_id)
+values
+('Small One', 'The small one turning fast', 'Lake', 125, '2000-01-01', '2014-01-01', 12);
+
+insert into ferris_wheels (name, description, location, speed,
+installation_date, maintenance_date, user_id)
+values
+('Big One', 'The big one turning slow', 'Island', 25, '2000-01-01', '2014-01-01', 12);
---
+++ Resources/FerrisWheelManager/Database/USERS.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `USERS` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+insert into USERS (username, password, role) values 
+('user', 'ferris', 'User');
+
+insert into USERS (username, password, role) values 
+('manager', 'wheel', 'Admin');
+
+insert into USERS (username, password, role) values 
+('Marvin', 'ferris', 'User');
+
+insert into USERS (username, password, role) values 
+('Zaphod', 'ferris', 'User');
