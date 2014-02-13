a76af49417edd3d749c618355268e30e8c1c47a4
Dominik Schadow <dominikschadow@googlemail.com>
2014-02-13 18:05:56 +0100

    Updated ferris_wheels script

--- Resources/FerrisWheelManager/Database/FERRIS_WHEELS.sql
+++ Resources/FerrisWheelManager/Database/FERRIS_WHEELS.sql
@@ -9,16 +9,16 @@ CREATE TABLE `FERRIS_WHEELS` (
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  CONSTRAINT `ferris_wheel_owner` FOREIGN KEY (`id`) REFERENCES `USERS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `ferris_wheel_owner` FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 
 insert into ferris_wheels (name, description, location, speed,
 installation_date, maintenance_date, user_id)
 values
-('Small One', 'The small one turning fast', 'Lake', 125, '2000-01-01', '2014-01-01', 12);
+('Small One', 'The small one turning fast', 'Lake', 125, '2000-01-01', '2014-01-01', (select id from users where username = 'Marvin'));
 
 insert into ferris_wheels (name, description, location, speed,
 installation_date, maintenance_date, user_id)
 values
-('Big One', 'The big one turning slow', 'Island', 25, '2000-01-01', '2014-01-01', 12);
+('Big One', 'The big one turning slow', 'Island', 25, '2000-01-01', '2014-01-01', (select id from users where username = 'Marvin'));
