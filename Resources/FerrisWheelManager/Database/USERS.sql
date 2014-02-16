3ccb14013cb861fe0e7f7cc5b00e43f627d89ab8
Dominik Schadow <dominikschadow@googlemail.com>
2014-02-16 17:34:40 +0100

    Added unit

--- Resources/FerrisWheelManager/Database/USERS.sql
+++ Resources/FerrisWheelManager/Database/USERS.sql
@@ -4,13 +4,14 @@ CREATE TABLE `USERS` (
   `password` varchar(128) NOT NULL,
   `role` varchar(20) NOT NULL,
   `salt` varchar(20) NOT NULL,
+  `unit` varchar(5) NOT NULL DEFAULT 'kmh',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
-insert into USERS (username, password, role, salt) values 
-('Marvin', 'aed5d790cb9bdb16a89c3c25a0d9b92282e17bf033879898b04922ff34a3cc8cc0b56cefb791edbdce1614ab85cd2bc790cd28b20bdebac6273c4a723d3da17c', 'Manager', 'Marvin');
+insert into USERS (username, password, role, salt, unit) values 
+('Marvin', 'aed5d790cb9bdb16a89c3c25a0d9b92282e17bf033879898b04922ff34a3cc8cc0b56cefb791edbdce1614ab85cd2bc790cd28b20bdebac6273c4a723d3da17c', 'Manager', 'Marvin', 'mph');
 
-insert into USERS (username, password, role, salt) values 
-('Zaphod', '736addab2f100eb7be2b0494a0316fbc3298b280bd2dc0c8b817654a7193149a625370072f6674f63cc8d58b82f225ed697df26ef6518f60da2900459779cfb0', 'User', 'Zaphod');
+insert into USERS (username, password, role, salt, unit) values 
+('Zaphod', '736addab2f100eb7be2b0494a0316fbc3298b280bd2dc0c8b817654a7193149a625370072f6674f63cc8d58b82f225ed697df26ef6518f60da2900459779cfb0', 'User', 'Zaphod', 'kmh');
