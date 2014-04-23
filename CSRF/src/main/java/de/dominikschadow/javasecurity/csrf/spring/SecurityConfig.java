b7da168b562b19c983e3995218c3f5aa5eb4f2fa
Dominik Schadow <dominikschadow@googlemail.com>
2014-04-23 21:31:30 +0200

    Spring Security added

---
+++ CSRF/src/main/java/de/dominikschadow/javasecurity/csrf/spring/SecurityConfig.java
@@ -0,0 +1,16 @@
+package de.dominikschadow.javasecurity.csrf.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+}
---
+++ CSRF/src/main/java/de/dominikschadow/javasecurity/csrf/spring/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package de.dominikschadow.javasecurity.csrf.spring;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    public SecurityWebApplicationInitializer() {
+        super(SecurityConfig.class);
+    }
+}
