38b5d586b5c7fee56ffdc38b6328bb6edf07c710
Dominik Schadow <dominikschadow@googlemail.com>
2014-04-28 17:43:11 +0200

    Initial version of spring crypto with jasypt project

---
+++ crypto-spring/src/main/java/de/dominikschadow/javasecurity/controller/IndexController.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of Java-Web-Security
+.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.javasecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 
+ * @author Dominik Schadow
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public ModelAndView showContacts() {
+        return new ModelAndView("index");
+    }
+}
---
+++ crypto-spring/src/main/java/de/dominikschadow/javasecurity/spring/CryptoSpringInitializer.java
@@ -0,0 +1,20 @@
+package de.dominikschadow.javasecurity.spring;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class CryptoSpringInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext container) {
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocations(new String[] {"/WEB-INF/applicationContext.xml", "/WEB-INF/dispatcher-servlet.xml"});
+
+        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
