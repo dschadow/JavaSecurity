d1f81164a81368a2892ad60e8b4949ac936c5735
Dominik Schadow <dominikschadow@googlemail.com>
2014-06-09 10:56:14 +0200

    Added user security constraint

--- session-handling-spring-security/src/main/java/de/dominikschadow/javasecurity/sessionhandling/spring/SecurityConfig.java
+++ session-handling-spring-security/src/main/java/de/dominikschadow/javasecurity/sessionhandling/spring/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/*", "/resources/**").permitAll()
+            .antMatchers("/user/**").hasRole("USER")
             .antMatchers("/admin/**").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and().formLogin();
