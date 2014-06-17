0dd85b46d9f7c6ecb1e4fce7373acfd968354b5c
Dominik Schadow <dominikschadow@googlemail.com>
2014-06-17 09:16:05 +0200

    Feedback servlet processing the sample form

---
+++ security-header/src/main/java/de/dominikschadow/javasecurity/header/feedback/FeedbackServlet.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2014 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Java Security project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.javasecurity.header.feedback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Simple feedback servlet which returns a success message.
+ *
+ * @author Dominik Schadow
+ */
+@WebServlet(name = "FeedbackServlet", urlPatterns = {"/FeedbackServlet"})
+public class FeedbackServlet extends HttpServlet {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        logger.info("Sending feedback...");
+
+        response.setContentType("text/html");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"resources/css/styles.css\" />");
+            out.println("<title>Security Response Header</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Feedback sent</h1>");
+            out.println("<div><a href=\"index.jsp\">Home</a></div>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+}
