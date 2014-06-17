9c5c481ce8f145a8c52795715ed81b91cb3660df
Dominik Schadow <dominikschadow@googlemail.com>
2014-06-17 14:41:41 +0200

    Refactoring

--- security-header/src/main/java/de/dominikschadow/javasecurity/header/servlets/LoginServlet.java
+++ security-header/src/main/java/de/dominikschadow/javasecurity/header/servlets/LoginServlet.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.javasecurity.header.feedback;
+package de.dominikschadow.javasecurity.header.servlets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
