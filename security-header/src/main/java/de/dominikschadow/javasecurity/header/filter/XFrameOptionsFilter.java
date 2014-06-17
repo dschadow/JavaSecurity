27bdaf8ba9f3304a4cb1cc80e0e9f093921043af
Dominik Schadow <dominikschadow@googlemail.com>
2014-06-17 15:26:29 +0200

    Added X-Frame-Options filter

---
+++ security-header/src/main/java/de/dominikschadow/javasecurity/header/filter/XFrameOptionsFilter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2014 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Java Security project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.javasecurity.header.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * This servlet filter protects the {@code protectedForm.jsp} against clickjacking by adding the {@code X-Frame-Options}
+ * header to the response. The {@code urlPatterns} should be far more wildcard in a real web application than in this
+ * demo project.
+ *
+ * @author Dominik Schadow
+ */
+@WebFilter(filterName = "XFrameOptionsFilter", urlPatterns = {"/forms/protectedForm.jsp"})
+public class XFrameOptionsFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        logger.info("XFrameOptionsFilter header added to response");
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("X-Frame-Options", "DENY");
+//        response.addHeader("X-Frame-Options", "SAMEORIGIN");
+//        response.addHeader("X-Frame-Options", "ALLOW-FROM http://localhost:8080");
+
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
