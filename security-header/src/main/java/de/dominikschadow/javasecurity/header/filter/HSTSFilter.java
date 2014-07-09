30289b827f8a770c0adf31b9c0234da919c59b25
Dominik Schadow <dominikschadow@googlemail.com>
2014-07-09 23:26:25 +0200

    HSTS header sample

---
+++ security-header/src/main/java/de/dominikschadow/javasecurity/header/filter/HSTSFilter.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2014 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Java Security project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.javasecurity.header.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * This servlet filter protects the complete page by forcing HTTPS usage.
+ *
+ * @author Dominik Schadow
+ */
+@WebFilter(filterName = "HSTSFilter", urlPatterns = {"/*"})
+public class HSTSFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        logger.info("HSTSFilter header added to response");
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Strict-Transport-Security", "max-age=2592000; includeSubDomains");
+
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
