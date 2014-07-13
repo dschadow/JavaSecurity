2bf2a7a0dcecbe0c36825c1d14d255c00fcb13e0
Dominik Schadow <dominikschadow@googlemail.com>
2014-07-13 10:48:34 +0200

    Refactoring

--- crypto-java/src/main/java/de/dominikschadow/javasecurity/symmetric/AESEncryptionSample.java
+++ crypto-java/src/main/java/de/dominikschadow/javasecurity/symmetric/AESEncryptionSample.java
@@ -35,7 +35,7 @@ import java.security.cert.CertificateException;
 
 /**
  * Symmetric encryption sample with plain Java. Loads the AES key from the sample keystore, encrypts and decrypts sample text with it.
- * Note that the <code>initializationVector</code> is not stored. One possibility to store it is to prepend it to the encrypted
+ * Note that the <code>INITIALIZATION_VECTOR</code> is not stored. One possibility to store it is to prepend it to the encrypted
  * message with a delimiter (all in Base64 encoding): <code>Base64(IV) + DELIMITER + Base64(ENCRYPTED MESSAGE)</code>
  * <p>
  * Uses Google Guava to Base64 print the encrypted message as readable format.
@@ -47,7 +47,7 @@ public class AESEncryptionSample {
     private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
     private static final String KEYSTORE_PATH = "/samples.ks";
     /** Non-secret initialization vector with 16 bytes (publicly exchanged between participants), may be a random number changed every time or a counter. */
-    private static final byte[] initializationVector = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3};
+    private static final byte[] INITIALIZATION_VECTOR = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3};
 
     public static void main(String[] args) {
         AESEncryptionSample ses = new AESEncryptionSample();
@@ -55,7 +55,7 @@ public class AESEncryptionSample {
         final char[] keystorePassword = "samples".toCharArray();
         final String keyAlias = "symmetric-sample";
         final char[] keyPassword = "symmetric-sample".toCharArray();
-        final IvParameterSpec iv = new IvParameterSpec(initializationVector);
+        final IvParameterSpec iv = new IvParameterSpec(INITIALIZATION_VECTOR);
 
         try {
             KeyStore ks = ses.loadKeystore(KEYSTORE_PATH, keystorePassword);
