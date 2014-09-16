98dd8b259d96f453e5379ff353eefe2ce7765e4a
Dominik Schadow <dominikschadow@googlemail.com>
2014-09-16 22:43:52 +0200

    Updated sizes and Javadoc

--- crypto-java/src/main/java/de/dominikschadow/javasecurity/hash/PBKDF2HashSample.java
+++ crypto-java/src/main/java/de/dominikschadow/javasecurity/hash/PBKDF2HashSample.java
@@ -37,9 +37,11 @@ import java.security.spec.InvalidKeySpecException;
 public class PBKDF2HashSample {
     private static final Logger logger = LoggerFactory.getLogger(PBKDF2HashSample.class);
     private static final String ALGORITHM = "PBKDF2WithHmacSHA1";
-    private static final int ITERATIONS = 1000000;
-    private static final int SALT_SIZE = 24;
-    private static final int HASH_SIZE = 192;
+    // calculation should require about 0,5 seconds, increase with faster hardware
+    private static final int ITERATIONS = 150000;
+    // salt size at least 32 bit
+    private static final int SALT_SIZE = 32;
+    private static final int HASH_SIZE = 512;
 
     public static void main(String[] args) {
         PBKDF2HashSample hs = new PBKDF2HashSample();
