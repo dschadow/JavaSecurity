d8d754cc6f04e1fcc1aa02331866bc4f3f53419f
Dominik Schadow <dominikschadow@googlemail.com>
2015-04-08 10:09:02 +0200

    Updated  configuration, no login required any more

--- CSRF-spring-security/src/main/java/de/dominikschadow/javasecurity/csrf/spring/SecurityConfig.java
+++ CSRF-spring-security/src/main/java/de/dominikschadow/javasecurity/csrf/spring/SecurityConfig.java
@@ -17,9 +17,8 @@
  */
 package de.dominikschadow.javasecurity.csrf.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -32,8 +31,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("csrf").password("csrf").roles("USER");
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
     }
 }
