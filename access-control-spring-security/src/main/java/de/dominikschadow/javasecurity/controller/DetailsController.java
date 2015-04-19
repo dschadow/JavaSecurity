660011874d78fc57957fd663c02046e29ffacd79
Dominik Schadow <dominikschadow@googlemail.com>
2015-04-19 16:27:27 +0200

    Import cleanup

--- access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/controller/DetailsController.java
+++ access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/controller/DetailsController.java
@@ -30,7 +30,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
