8fe26567aef7fffb1caa95441c1e0ef7c43b0341
Dominik Schadow <dominikschadow@googlemail.com>
2015-04-19 14:14:22 +0200

    Organized imports

--- access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/controller/ListController.java
+++ access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/controller/ListController.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -30,9 +29,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
