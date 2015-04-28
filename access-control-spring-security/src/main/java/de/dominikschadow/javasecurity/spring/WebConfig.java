3f6eaf104a4fc92a332068815ab72934d2d43627
Dominik Schadow <dominikschadow@googlemail.com>
2015-04-28 21:20:20 +0200

    Added service package to ComponentScan annotation

--- access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/spring/WebConfig.java
+++ access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/spring/WebConfig.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
