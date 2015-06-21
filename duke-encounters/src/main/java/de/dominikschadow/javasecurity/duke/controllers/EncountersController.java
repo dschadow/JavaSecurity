56061ff43c4b2fa84ee5932804d4254f342848c6
Dominik Schadow <dominikschadow@googlemail.com>
2015-06-21 15:04:31 +0200

    Extended Controllers

--- duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/controllers/EncountersController.java
+++ duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/controllers/EncountersController.java
@@ -21,26 +21,36 @@ import de.dominikschadow.javasecurity.duke.services.EncounterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
-public class HomeController {
+@RequestMapping(value = "/encounters")
+public class EncountersController {
     private EncounterService encounterService;
 
     @Autowired
-    public HomeController(EncounterService encounterService) {
+    public EncountersController(EncounterService encounterService) {
         this.encounterService = encounterService;
     }
 
-    @RequestMapping(value = "/", method= GET)
-    public String home(Model model) {
+    @RequestMapping(method = GET)
+    public String getAllEncounters(Model model) {
 
-        return "index";
+        return "encounters";
     }
 
-    @RequestMapping(value = "search", method = GET)
-    public String searchEncounters(Model model) {
-        return "search";
+    @RequestMapping(params = "type", method = GET)
+    public String getEncountersByType(@RequestParam("type") String type, Model model) {
+        return "encounters";
+    }
+
+    @RequestMapping(value = "/{id}", method = GET)
+    public String getEncounter(@PathVariable("id") long id, Model model) {
+
+        return "encounterDetails";
     }
 }
