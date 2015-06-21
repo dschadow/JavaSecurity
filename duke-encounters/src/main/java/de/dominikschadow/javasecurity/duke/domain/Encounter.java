b363eda9c603e9f485c5dc6208e252b8bbe124ed
Dominik Schadow <dominikschadow@googlemail.com>
2015-06-21 09:44:56 +0200

    Added basic entities

---
+++ duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/domain/Encounter.java
@@ -0,0 +1,94 @@
+package de.dominikschadow.javasecurity.duke.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+public class Encounter {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotNull
+    @Column(nullable = false)
+    private String event;
+    @NotNull
+    @Column(nullable = false)
+    private String location;
+    @NotNull
+    @Column(nullable = false)
+    private String country;
+    private String comment;
+    @NotNull
+    @Column(nullable = false)
+    private LocalDate date;
+    private User user;
+    private int confirmations;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getConfirmations() {
+        return confirmations;
+    }
+
+    public void setConfirmations(int confirmations) {
+        this.confirmations = confirmations;
+    }
+}
---
+++ duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/domain/User.java
@@ -0,0 +1,83 @@
+package de.dominikschadow.javasecurity.duke.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotNull
+    @Column(nullable = false)
+    private String username;
+    private String firstname;
+    private String lastname;
+    @NotNull
+    @Column(nullable = false)
+    private String email;
+    @NotNull
+    @Column(nullable = false)
+    private String password;
+    private LocalDateTime registrationDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
