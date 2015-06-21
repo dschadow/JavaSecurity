98219ea472ad165d5e3a24b648d41cb403033b2f
Dominik Schadow <dominikschadow@googlemail.com>
2015-06-21 09:20:06 +0200

    Refactored application intrusion detection sample project to Duke Encounters

---
+++ duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/DukeEncountersApplication.java
@@ -0,0 +1,12 @@
+package de.dominikschadow.javasecurity.duke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukeEncountersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DukeEncountersApplication.class, args);
+    }
+}
