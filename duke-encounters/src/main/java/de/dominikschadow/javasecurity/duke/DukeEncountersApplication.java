8d198020acfd8653cfd3f545f7e09bec31a08f40
Dominik Schadow <dominikschadow@googlemail.com>
2015-06-21 13:33:03 +0200

    Cleanup

--- duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/DukeEncountersApplication.java
+++ duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/DukeEncountersApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DukeEncountersApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DukeEncountersApplication.class, args);
     }
