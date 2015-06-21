103ec6204a9b3eda0275844d724c7591fed470bb
Dominik Schadow <dominikschadow@googlemail.com>
2015-06-21 13:33:15 +0200

    Basic controller

--- duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/controllers/HomeController.java
+++ duke-encounters/src/main/java/de/dominikschadow/javasecurity/duke/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class HomeController {
@@ -16,7 +16,7 @@ public class HomeController {
         this.encounterService = encounterService;
     }
 
-    @RequestMapping(value = "/", method= RequestMethod.GET)
+    @RequestMapping(value = "/", method= GET)
     public String home(Model model) {
 
         return "index";
