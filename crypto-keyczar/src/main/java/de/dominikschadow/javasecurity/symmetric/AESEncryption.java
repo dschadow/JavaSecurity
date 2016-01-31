d1e95be0e9812e11a70299c3ad29cd34f4d3dade
Dominik Schadow <dominikschadow@googlemail.com>
2016-01-31 16:29:02 +0100

    Added private constructors

--- crypto-keyczar/src/main/java/de/dominikschadow/javasecurity/symmetric/AESEncryption.java
+++ crypto-keyczar/src/main/java/de/dominikschadow/javasecurity/symmetric/AESEncryption.java
@@ -32,14 +32,19 @@ public class AESEncryption {
     private static final Logger LOGGER = LoggerFactory.getLogger(AESEncryption.class);
     private static final String KEYSET_PATH = "crypto-keyczar/src/main/resources/key-sets/encrypt/symmetric";
 
+    /**
+     * Private constructor.
+     */
+    private AESEncryption() {
+    }
+
     public static void main(String[] args) {
-        AESEncryption res = new AESEncryption();
         final String initialText = "Some dummy text for encryption";
         try {
-            String ciphertext = res.encrypt(initialText);
-            String plaintext = res.decrypt(ciphertext);
+            String ciphertext = encrypt(initialText);
+            String plaintext = decrypt(ciphertext);
 
-            res.printReadableMessages(initialText, ciphertext, plaintext);
+            printReadableMessages(initialText, ciphertext, plaintext);
         } catch (KeyczarException ex) {
             LOGGER.error(ex.getMessage(), ex);
         }
@@ -52,17 +57,17 @@ public class AESEncryption {
      * @return The encrypted text (in Base64)
      * @throws KeyczarException
      */
-    private String encrypt(String initialText) throws KeyczarException {
+    private static String encrypt(String initialText) throws KeyczarException {
         Crypter crypter = new Crypter(KEYSET_PATH);
         return crypter.encrypt(initialText);
     }
 
-    private String decrypt(String ciphertext) throws KeyczarException {
+    private static String decrypt(String ciphertext) throws KeyczarException {
         Crypter crypter = new Crypter(KEYSET_PATH);
         return crypter.decrypt(ciphertext);
     }
 
-    private void printReadableMessages(String initialText, String ciphertext, String plaintext) {
+    private static void printReadableMessages(String initialText, String ciphertext, String plaintext) {
         LOGGER.info("initialText: {}", initialText);
         LOGGER.info("cipherText as Base64: {}", ciphertext);
         LOGGER.info("plaintext: {}", plaintext);
