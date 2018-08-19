2d70c4568815c86ed0393e851a4775c597af7c73
Dominik Schadow <dominikschadow@gmail.com>
2018-08-19 12:44:39 +0200

    Migrated to Spring Boot 2

--- sso-with-github/src/main/java/de/dominikschadow/javasecurity/Application.java
+++ sso-with-github/src/main/java/de/dominikschadow/javasecurity/Application.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Java Security project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.dominikschadow.javasecurity;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +23,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Requires the configuration of @code{githubClient.client.clientId} and @code{githubClient.client.clientSecret} as runtime parameter.
  * This project is based on the Spring Boot and OAuth2 tutorial available at https://spring.io/guides/tutorials/spring-boot-oauth2
+ *
+ * @author Dominik Schadow
  */
 @SpringBootApplication
 public class Application {
