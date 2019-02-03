6d1c1c9959b8bdf134bc25810a0e18206558f4aa
Dominik Schadow <dominikschadow@gmail.com>
2019-02-03 17:13:10 +0100

    Added tests

---
+++ csrf-spring-security/src/test/java/de/dominikschadow/javasecurity/csrf/orders/OrderControllerTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 Dominik Schadow, dominikschadow@gmail.com
+ *
+ * This file is part of the Java Security project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.dominikschadow.javasecurity.csrf.orders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testWithCsrfToken() throws Exception {
+        mockMvc.perform(post("/order").with(csrf())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "My Item"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(content().string(containsString("You have ordered the following item:")));
+    }
+
+    @Test
+    public void testWithoutCsrfToken() throws Exception {
+        mockMvc.perform(post("/order")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "My Item"))
+                .andExpect(status().isForbidden());
+    }
+}
