2d1354bf29e08ec66a9f62633e0bf3772496050a
Dominik Schadow <dominikschadow@gmail.com>
2019-12-25 18:06:25 +0100

    Turned web application into a Spring Boot web application
    Removed Tomcat and Servlet code, added Spring Boot and Thymeleaf

--- security-logging/src/main/java/de/dominikschadow/javasecurity/logging/home/HomeController.java
+++ security-logging/src/main/java/de/dominikschadow/javasecurity/logging/home/HomeController.java
@@ -15,33 +15,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.javasecurity.logging.servlets;
+package de.dominikschadow.javasecurity.logging.home;
 
 import org.owasp.security.logging.SecurityMarkers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
- * Simple login servlet which returns a success message and logs security relevant events into the log file.
+ * Simple login controller which returns a success message and logs security relevant events into the log file.
  *
  * @author Dominik Schadow
  */
-@WebServlet(name = "LoginServlet", urlPatterns = "/LoginServlet")
-public class LoginServlet extends HttpServlet {
-    private static final long serialVersionUID = -660893987741671511L;
-    private static final Logger log = LoggerFactory.getLogger(LoginServlet.class);
+@Controller
+public class HomeController {
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("login", new Login());
+
+        return "index";
+    }
+
+    @PostMapping("login")
+    public String firstTask(Login login, Model model) {
+        String username = login.getUsername();
+        String password = login.getPassword();
 
         log.info(SecurityMarkers.CONFIDENTIAL, "User {} with password {} logged in", username, password);
         log.info(SecurityMarkers.EVENT_FAILURE, "User {} with password {} logged in", username, password);
@@ -53,21 +56,8 @@ public class LoginServlet extends HttpServlet {
         log.info(SecurityMarkers.SECURITY_SUCCESS, "User {} with password {} logged in", username, password);
         log.info(SecurityMarkers.TOP_SECRET, "User {} with password {} logged in", username, password);
 
-        response.setContentType("text/html; charset=UTF-8");
+        model.addAttribute("login", login);
 
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"../resources/css/styles.css\" />");
-            out.println("<title>Security Logging</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Login successful</h1>");
-            out.println("<div><a href=\"/security-logging\">Home</a></div>");
-            out.println("</body>");
-            out.println("</html>");
-        } catch (IOException ex) {
-            log.error(ex.getMessage(), ex);
-        }
+        return "login";
     }
 }
---
+++ security-logging/src/main/java/de/dominikschadow/javasecurity/logging/home/Login.java
@@ -0,0 +1,22 @@
+package de.dominikschadow.javasecurity.logging.home;
+
+public class Login {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
