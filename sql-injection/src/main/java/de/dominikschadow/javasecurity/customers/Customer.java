92845ee8ec5a7de338d7d84e2d861ace92613081
Dominik Schadow <mail@dominikschadow.de>
2021-12-27 17:13:34 +0100

    refactored package name

--- sql-injection/src/main/java/de/dominikschadow/javasecurity/customers/Customer.java
+++ sql-injection/src/main/java/de/dominikschadow/javasecurity/customers/Customer.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.javasecurity.queries;
+package de.dominikschadow.javasecurity.customers;
 
 import lombok.Getter;
 import lombok.Setter;
