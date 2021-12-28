e758436b65a7740cd3246fea21c3922de6c332cf
Dominik Schadow <mail@dominikschadow.de>
2021-12-28 16:28:15 +0100

    cleaned up package structure

--- session-handling-spring-security/src/main/java/de/dominikschadow/javasecurity/sessionhandling/Application.java
+++ session-handling-spring-security/src/main/java/de/dominikschadow/javasecurity/sessionhandling/Application.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.javasecurity;
+package de.dominikschadow.javasecurity.sessionhandling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- session-handling-spring-security/src/test/java/de/dominikschadow/javasecurity/sessionhandling/ApplicationTest.java
+++ session-handling-spring-security/src/test/java/de/dominikschadow/javasecurity/sessionhandling/ApplicationTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.dominikschadow.javasecurity;
+package de.dominikschadow.javasecurity.sessionhandling;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
