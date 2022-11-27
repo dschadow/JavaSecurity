33fbcb4e265d04c6fe828ed225508d8dfc76162a
Dominik Schadow <mail@dominikschadow.de>
2022-11-27 15:41:06 +0100

    imports to jakarta

--- sql-injection/src/main/java/de/dominikschadow/javasecurity/customers/Customer.java
+++ sql-injection/src/main/java/de/dominikschadow/javasecurity/customers/Customer.java
@@ -17,12 +17,11 @@
  */
 package de.dominikschadow.javasecurity.customers;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "customers")
