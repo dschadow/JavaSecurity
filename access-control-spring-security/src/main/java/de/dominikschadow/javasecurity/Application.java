b75afe5746054c1631037e61d802dadef634eded
Dominik Schadow <mail@dominikschadow.de>
2022-11-27 17:06:24 +0100

    upgraded project to make use of spring boot 3

--- access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/Application.java
+++ access-control-spring-security/src/main/java/de/dominikschadow/javasecurity/Application.java
@@ -19,10 +19,8 @@ package de.dominikschadow.javasecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,6 +30,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author Dominik Schadow
  */
 @SpringBootApplication
+@EnableWebSecurity
 @Configuration
 public class Application implements WebMvcConfigurer {
     public static void main(String[] args) {
@@ -41,14 +40,5 @@ public class Application implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-    }
-
-    /**
-     * BCryptPasswordEncoder takes a work factor as first argument. The default is 10, the valid range is 4 to 31. The
-     * amount of work increases exponentially.
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
     }
 }
