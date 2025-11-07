449d103b93b6a75f60e01a7902fd95b733261870
Dominik Schadow <mail@dominikschadow.de>
2025-11-07 21:49:17 +0100

    replaced deprecated API usage

--- csp-spring-security/src/main/java/de/dominikschadow/javasecurity/SecurityConfig.java
+++ csp-spring-security/src/main/java/de/dominikschadow/javasecurity/SecurityConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Dominik Schadow, dominikschadow@gmail.com
+ * Copyright (C) 2025 Dominik Schadow, dominikschadow@gmail.com
  *
  * This file is part of the Java Security project.
  *
@@ -33,8 +33,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // @formatter:off
         http
-            .headers()
-                .contentSecurityPolicy("default-src 'self'");
+            .headers(headers -> headers
+                    .contentSecurityPolicy(csp -> csp
+                            .policyDirectives("default-src 'self'"))
+            );
         // @formatter:on
 
         return http.build();
