f6fc9f4095b3eed570c1463c3f1852c9aa29ef0a
Dominik Schadow <mail@dominikschadow.de>
2025-11-07 21:40:49 +0100

    replaced deprecated API usage

--- csrf-spring-security/src/main/java/de/dominikschadow/javasecurity/SecurityConfig.java
+++ csrf-spring-security/src/main/java/de/dominikschadow/javasecurity/SecurityConfig.java
@@ -19,6 +19,7 @@ package de.dominikschadow.javasecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,11 +33,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-            .httpBasic()
-                .disable();
-        // @formatter:on
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
     }
