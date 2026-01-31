f368a0a9385ba55184b9bb80a528db934bfddc8d
Dominik Schadow <mail@dominikschadow.de>
2026-01-31 17:22:05 +0100

    added missing file

---
+++ session-handling-spring-security/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+create table users(
+    username varchar(50) not null primary key,
+    password varchar(500) not null,
+    enabled boolean not null
+);
+
+create table authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+);
+
+create unique index ix_auth_username on authorities (username,authority);
